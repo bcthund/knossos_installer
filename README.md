76424bb464c8cd5360ce6ae8964860663425325f
bcthund <bthundereagle@gmail.com>
2020-07-02 22:24:13 -0400

    Script and README update

--- README.md
+++ README.md
@@ -1,1 +1,31 @@
 # knossos_installer
+This is a script I created for a system restore procedure during a fresh install. When upgrading from Ubuntu 18.04 to Kubuntu 20.04 I found that knossos did not have a release file. So I decided to build from source but I carefully documented and scripted the steps as part of a larger script that reinstalls all of my common programs and settings.
+
+Each stage of the script will confirm if you want to continue, giving you the option to stop should errors occur.
+
+The script will ask where you want to install, it is recommended to keep the default. The files will be copied and built in-place. You will be given the option to install the knossos version included with this script or pull the latest version.
+
+# Packages for Reference (installed automatically):
+<pre>
+  nodejs
+  npm
+  python3-wheel
+  python3-setuptools
+  pyqt5-dev
+  pyqt5-dev-tools
+  qttools5-dev-tools
+  qt5-default
+  pipenv
+  yarn
+  ninja-build
+</pre>
+
+# Addons
+This is a launcher for Freespace Open and game created using the FSO engine. The launcher has the ability to add and remove games. The following is a list of my preferred games.
+
+<pre>
+  Freespace Open
+  Freespace 2
+  Diaspora
+  Solaris
+</pre>
--- knossos.sh
+++ knossos.sh
@@ -17,6 +17,9 @@ cyan='\033[0;36m'
 CYAN='\033[1;36m'
 NC='\033[0m'
 
+# Save the working directory of the script
+working_dir=$PWD
+
 if [ "$1" != "${1#[debug]}" ] ;then
     cmd(){ echo ">> ${WHITE}$1${NC}"; }
     echo "${RED}DEBUG: Commands will be echoed to console${NC}"
@@ -38,6 +41,48 @@ read answer
 echo
 if [ "$answer" != "${answer#[Yy]}" ] ;then
 
+    # Set Install Directory
+        install_dir="$HOME/knossos"
+        echo
+        printf "${BLUE}Where do you want to install to?${NC}\n"
+        printf "${YELLOW}\t0) $HOME/knossos (default)${NC}\n"
+        printf "${YELLOW}\t1) $HOME/Games/knossos${NC}\n"
+        printf "${YELLOW}\t2) Custom${NC}\n"
+        echo
+        printf "${CYAN}Selection? ${NC}"
+        read install
+        if [ "$install" != "${install#[1]}" ] ;then install_dir="$HOME/Games/knossos"; fi
+        if [ "$install" != "${install#[2]}" ] ;then printf "${BLUE}Directory? ${NC}"; read install_dir; fi
+        
+        # Create Directories
+        echo
+        #cmd "sudo cp --preserve=all -r ./src/knossos-develop ./src/knossos_tmp/build"
+        #cmd "mkdir -pv ./src/knossos_tmp/build"
+        
+        remdir="n"
+        if [ -d "$install_dir" ] ;then
+            printf "${BLUE}Directory already exists, remove first? ${NC}"
+            read answer
+            if [ "$answer" != "${answer#[Yy]}" ] ;then
+                cmd "sudo rm -rf $install_dir"
+                remdir="y"
+            fi
+        fi
+        
+        if [ ! -d "$install_dir" ] ;then
+        
+            if [ "$remdir" != "${remdir#[Nn]}" ] ;then
+                printf "${BLUE}Directory '$install_dir' doesn't exist, create (y/n)? ${NC}"
+                read answer
+            fi
+            if [ "$answer" != "${answer#[Yy]}" ] || [ "$remdir" != "${remdir#[Yy]}" ] ;then
+                cmd "mkdir -pv $install_dir"
+            else
+                printf "${RED}Aborting!${NC}\n"
+                exit 0
+            fi
+        fi
+
     # Dependencies
         echo
         printf "${BLUE}Add key for yarn${NC}"
@@ -48,51 +93,40 @@ if [ "$answer" != "${answer#[Yy]}" ] ;then
         echo
         printf "${BLUE}Add yarn source${NC}"
         echo -n " (y/n)? "; read answer; if [ "$answer" != "${answer#[Yy]}" ] ;then
-            echo "echo \"deb [trusted=yes] https://dl.yarnpkg.com/debian/ stable main\" | sudo tee /etc/apt/sources.list.d/yarn.list"
             cmd "echo "deb [trusted=yes] https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list"
         fi
         
         echo
         printf "${BLUE}Update apt${NC}"
         echo -n " (y/n)? "; read answer; if [ "$answer" != "${answer#[Yy]}" ] ;then
-            echo "sudo apt update"
             cmd "sudo apt update"
         fi
         
         echo
         printf "${BLUE}Install apt packages${NC}"
         echo -n " (y/n)? "; read answer; if [ "$answer" != "${answer#[Yy]}" ] ;then
-            echo "sudo apt install nodejs npm python3-wheel python3-setuptools pyqt5-dev pyqt5-dev-tools qttools5-dev-tools qt5-default pipenv yarn ninja-build"
             cmd "sudo apt install nodejs npm python3-wheel python3-setuptools pyqt5-dev pyqt5-dev-tools qttools5-dev-tools qt5-default pipenv yarn ninja-build"
         fi
-
-    # Create Directories
-        echo
-        printf "${BLUE}Creating tmp directory${NC}\n"
-        #cmd "sudo cp --preserve=all -r ./src/knossos-develop ./src/knossos_tmp/build"
-        cmd "mkdir -pv ./src/knossos_tmp/build"
     
     # Grab Source
         echo
         echo -n "${BLUE}Pull current source from git (requires internet connection) (y/n)? ${NC}"
         read source
         if [ "$source" != "${source#[Yy]}" ] ;then
-            #cmd "git clone https://github.com/ngld/knossos.git ./src/knossos_tmp/git"
-            cmd "git clone https://github.com/ngld/knossos.git ./src/knossos_tmp/build"
+            cmd "git clone https://github.com/ngld/knossos.git $install_dir"
         else
-            #cmd "ln -sr ./src/knossos-develop/ ./src/knossos_tmp/git"
-            cmd "sudo cp --preserve=all -rT ./src/knossos-develop ./src/knossos_tmp/build"
+            cmd "cp --preserve=all -rT ./src/knossos-develop $install_dir"
         fi
     
     # Knossos: build and install
         echo
-        printf "${BLUE}Moving into directory './src/knossos_tmp/build'${NC}\n"
-        cmd "cd ./src/knossos_tmp/build"
+        printf "${BLUE}Moving into directory '$install_dir'${NC}\n"
+        cmd "cd $install_dir"
+        cmd "echo $PWD"
         cmd "ls -al"
         ctrl_c() {
             echo;
-            cmd "cd ../../..";
-            cmd "rm -rf ./src/knossos_tmp";
+            cmd "cd $working_dir";
             echo;
             exit 0;
         }
@@ -100,7 +134,6 @@ if [ "$answer" != "${answer#[Yy]}" ] ;then
         echo
         printf "${BLUE}Install pipenv${NC}"
         echo -n " (y/n)? "; read answer; if [ "$answer" != "${answer#[Yy]}" ] ;then
-            echo "pipenv install"
             cmd "pipenv install"
         fi
     
@@ -114,7 +147,6 @@ if [ "$answer" != "${answer#[Yy]}" ] ;then
         echo
         printf "${BLUE}Install npm modules${NC}"
         echo -n " (y/n)? "; read answer; if [ "$answer" != "${answer#[Yy]}" ] ;then
-            echo "npm imstall"
             cmd "npm install"
         fi
         
@@ -122,7 +154,6 @@ if [ "$answer" != "${answer#[Yy]}" ] ;then
         printf "${BLUE}Configure/Build Knossos${NC}"
         echo -n " (y/n)? "; read answer; if [ "$answer" != "${answer#[Yy]}" ] ;then
             printf "${YELLOW}Watch for 'Writing build.ninja' for success.${NC}\n"
-            echo "pipenv run python configure.py"
             cmd "pipenv run python configure.py"
         fi
     
@@ -130,33 +161,15 @@ if [ "$answer" != "${answer#[Yy]}" ] ;then
         echo
         printf "${BLUE}Test Knossos${NC}"
         echo -n " (y/n)? "; read answer; if [ "$answer" != "${answer#[Yy]}" ] ;then
-            echo "ninja run"
             cmd "ninja run"
         fi
     
-    cmd "cd ../../.."
+    cmd "cd $working_dir";
     ctrl_c() {
         echo;
-        cmd "rm -rf ./src/knossos_tmp";
         echo;
         exit 0;
     }
-    
-    echo
-    printf "${BLUE}Install to ~/Games/knossos${NC}"
-    echo -n " (y/n)? "; read answer; if [ "$answer" != "${answer#[Yy]}" ] ;then
-        echo "sudo mv ./src/knossos_tmp /home/$USER/Games/knossos"
-        cmd "sudo mv ./src/knossos_tmp /home/$USER/Games/knossos"
-    else
-        # Removing build files
-        echo
-        printf "${BLUE}Remove './src/knossos_tmp'${NC}"
-        echo -n "${CYAN} (y/n)? ${NC}"; read answer; if [ "$answer" != "${answer#[Yy]}" ] ;then
-            cmd "rm -rf ./src/knossos_tmp";
-        fi
-    fi
-    
-    
     
     ctrl_c() { echo; echo; exit 0; }
     
