5a6a276ba00a58c8b7c249f66aae646e89c45fd8
bcthund <bthundereagle@gmail.com>
2020-07-06 22:48:13 -0400

    Changed from sh to bash

--- knossos.sh
+++ knossos.sh
@@ -1,20 +1,21 @@
-#!/bin/sh
-grey='\033[1;30m'
-red='\033[0;31m'
-RED='\033[1;31m'
-green='\033[0;32m'
-GREEN='\033[1;32m'
-yellow='\033[0;33m'
-YELLOW='\033[1;33m'
-purple='\033[0;35m'
-PURPLE='\033[1;35m'
-white='\033[0;37m'
-WHITE='\033[1;37m'
-blue='\033[0;34m'
-BLUE='\033[1;34m'
-cyan='\033[0;36m'
-CYAN='\033[1;36m'
-NC='\033[0m'
+#!/bin/bash
+grey='\e[37;0m'
+GREY='\e[37;1m'
+red='\e[31;0m'
+RED='\e[31;1m'
+green='\e[32;0m'
+GREEN='\e[32;1m'
+yellow='\e[33;0m'
+YELLOW='\e[33;1m'
+purple='\e[35;0m'
+PURPLE='\e[35;1m'
+white='\e[37;0m'
+WHITE='\e[37;1m'
+blue='\e[34;0m'
+BLUE='\e[34;1m'
+cyan='\e[36;0m'
+CYAN='\e[36;1m'
+NC='\e[39;0m'
 
 # Save the working directory of the script
 working_dir=$PWD
@@ -39,20 +40,20 @@ do
         shift # Remove --verbose from processing
         ;;
         -h|--help)
-        echo "${WHITE}"
-        echo "Usage: $0 <options>"
-        echo
-        echo "Options:"
-        echo "  -h, --help            show this help message and exit"
-        echo "  -v, --verbose         print commands being run before running them"
-        echo "  -d, --debug           print commands to be run but do not execute them"
-        echo "${NC}"
+        echo -e "${WHITE}"
+        echo -e "Usage: $0 <options>"
+        echo -e
+        echo -e "Options:"
+        echo -e "  -h, --help            show this help message and exit"
+        echo -e "  -v, --verbose         print commands being run before running them"
+        echo -e "  -d, --debug           print commands to be run but do not execute them"
+        echo -e "${NC}"
         exit
         shift # Remove from processing
         ;;
         *)
         OTHER_ARGUMENTS="$OTHER_ARGUMENTS$1 "
-        echo "${RED}Unknown argument: $1${NC}"
+        echo -e "${RED}Unknown argument: $1${NC}"
         exit
         shift # Remove generic argument from processing
         ;;
@@ -60,28 +61,28 @@ do
 done
 
 cmd(){
-    if [ "$VERBOSE" = true ] || [ "$DEBUG" = true ]; then echo ">> ${WHITE}$1${NC}"; fi;
+    if [ "$VERBOSE" = true ] || [ "$DEBUG" = true ]; then echo -e ">> ${WHITE}$1${NC}"; fi;
     if [ "$DEBUG" = false ]; then eval $1; fi;
 }
 
 # trap ctrl-c and call ctrl_c()
-ctrl_c() { echo; echo; exit 0; }
+ctrl_c() { echo -e; echo -e; exit 0; }
 trap ctrl_c INT
 
-echo
-echo -n "${PURPLE}Compile Knossos (y/n)? ${NC}"
+echo -e
+echo -e -n "${PURPLE}Compile Knossos (y/n)? ${NC}"
 read answer
-echo
+echo -e
 if [ "$answer" != "${answer#[Yy]}" ] ;then
 
     # Set Install Directory
         install_dir="$HOME/knossos"
-        echo
+        echo -e
         printf "${PURPLE}Source [knossos]: ${BLUE}Where do you want to install to?${NC}\n"
         printf "${YELLOW}\t0) $HOME/knossos (default)${NC}\n"
         printf "${YELLOW}\t1) $HOME/Games/knossos${NC}\n"
         printf "${YELLOW}\t2) Custom${NC}\n"
-        echo
+        echo -e
         printf "${GREEN}Selection? ${NC}"
         read install
         if [ "$install" != "${install#[1]}" ] ;then install_dir="$HOME/Games/knossos"; fi
@@ -112,28 +113,28 @@ if [ "$answer" != "${answer#[Yy]}" ] ;then
         fi
 
     # Dependencies
-        echo
+        echo -e
         printf "${PURPLE}Source [knossos]: ${BLUE}Add key for yarn${NC}"
-        echo -n "${GREEN} (y/n)? ${NC}"; read answer; if [ "$answer" != "${answer#[Yy]}" ] ;then
+        echo -e -n "${GREEN} (y/n)? ${NC}"; read answer; if [ "$answer" != "${answer#[Yy]}" ] ;then
             cmd "curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -"
         fi
         
-        echo
+        echo -e
         printf "${PURPLE}Source [knossos]: ${BLUE}Add yarn source and update apt${NC}"
-        echo -n "${GREEN} (y/n)? ${NC}"; read answer; if [ "$answer" != "${answer#[Yy]}" ] ;then
-            cmd "echo 'deb [trusted=yes] https://dl.yarnpkg.com/debian/ stable main' | sudo tee /etc/apt/sources.list.d/yarn.list"
+        echo -e -n "${GREEN} (y/n)? ${NC}"; read answer; if [ "$answer" != "${answer#[Yy]}" ] ;then
+            cmd "echo -e 'deb [trusted=yes] https://dl.yarnpkg.com/debian/ stable main' | sudo tee /etc/apt/sources.list.d/yarn.list"
             cmd "sudo apt update"
         fi
         
-        echo
+        echo -e
         printf "${PURPLE}Source [knossos]: ${BLUE}Install apt packages${NC}"
-        echo -n "${GREEN} (y/n)? ${NC}"; read answer; if [ "$answer" != "${answer#[Yy]}" ] ;then
+        echo -e -n "${GREEN} (y/n)? ${NC}"; read answer; if [ "$answer" != "${answer#[Yy]}" ] ;then
             cmd "sudo apt install nodejs npm python3-wheel python3-setuptools pyqt5-dev pyqt5-dev-tools qttools5-dev-tools qt5-default pipenv yarn ninja-build"
         fi
     
     # Grab Source
-        echo
-        echo -n "${PURPLE}Source [knossos]: ${BLUE}Pull current source from git (requires internet connection) (y/n)? ${NC}"
+        echo -e
+        echo -e -n "${PURPLE}Source [knossos]: ${BLUE}Pull current source from git (requires internet connection) (y/n)? ${NC}"
         read source
         if [ "$source" != "${source#[Yy]}" ] ;then
             cmd "git clone https://github.com/ngld/knossos.git $install_dir"
@@ -142,58 +143,58 @@ if [ "$answer" != "${answer#[Yy]}" ] ;then
         fi
     
     # Knossos: build and install
-        echo
+        echo -e
         printf "${PURPLE}Source [knossos]: ${BLUE}Moving into directory '$install_dir'${NC}\n"
         cmd "cd $install_dir"
-        cmd "echo $PWD"
+        cmd "echo -e $PWD"
         cmd "ls -al"
         ctrl_c() {
-            echo;
+            echo -e;
             cmd "cd '${working_dir}'";
-            echo;
+            echo -e;
             exit 0;
         }
     
-        echo
+        echo -e
         printf "${PURPLE}Source [knossos]: ${BLUE}Install pipenv${NC}"
-        echo -n "${GREEN} (y/n)? ${NC}"; read answer; if [ "$answer" != "${answer#[Yy]}" ] ;then
+        echo -e -n "${GREEN} (y/n)? ${NC}"; read answer; if [ "$answer" != "${answer#[Yy]}" ] ;then
             cmd "pipenv install"
         fi
     
         # Knossos says to do this but it doesn't work, also doesn't seem necessary
         #printf "${BLUE}Install yarn${NC}"
-        #echo -n "${GREEN} (y/n)? ${NC}"; read answer; if [ "$answer" != "${answer#[Yy]}" ] ;then
-        #    echo "yarn install"
+        #echo -e -n "${GREEN} (y/n)? ${NC}"; read answer; if [ "$answer" != "${answer#[Yy]}" ] ;then
+        #    echo -e "yarn install"
         #    cmd "yarn install"
         #fi
         
-        echo
+        echo -e
         printf "${PURPLE}Source [knossos]: ${BLUE}Install npm modules${NC}"
-        echo -n "${GREEN} (y/n)? ${NC}"; read answer; if [ "$answer" != "${answer#[Yy]}" ] ;then
+        echo -e -n "${GREEN} (y/n)? ${NC}"; read answer; if [ "$answer" != "${answer#[Yy]}" ] ;then
             cmd "npm install"
         fi
         
-        echo
+        echo -e
         printf "${PURPLE}Source [knossos]: ${BLUE}Configure/Build Knossos${NC}"
-        echo -n "${GREEN} (y/n)? ${NC}"; read answer; if [ "$answer" != "${answer#[Yy]}" ] ;then
+        echo -e -n "${GREEN} (y/n)? ${NC}"; read answer; if [ "$answer" != "${answer#[Yy]}" ] ;then
             printf "${YELLOW}Watch for 'Writing build.ninja' for success.${NC}\n"
             cmd "pipenv run python configure.py"
         fi
     
     # Test Run
-        echo
+        echo -e
         printf "${PURPLE}Source [knossos]: ${BLUE}Test Knossos${NC}"
-        echo -n "${GREEN} (y/n)? ${NC}"; read answer; if [ "$answer" != "${answer#[Yy]}" ] ;then
+        echo -e -n "${GREEN} (y/n)? ${NC}"; read answer; if [ "$answer" != "${answer#[Yy]}" ] ;then
             cmd "ninja run"
         fi
     
     cmd "cd '${working_dir}'";
     ctrl_c() {
-        echo;
-        echo;
+        echo -e;
+        echo -e;
         exit 0;
     }
     
-    ctrl_c() { echo; echo; exit 0; }
+    ctrl_c() { echo -e; echo -e; exit 0; }
     
 fi
